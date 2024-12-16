72fab72141356de5a8cf93ef887a0c67cd99bb29
Kaique Motta <kaique.motta@hotmail.com>
2024-12-16 01:35:53 -0300

    feat: dev

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 exports.handler = async (event) => {
-    if (event.triggerSource === "TokenGeneration_HostedAuth") {
-        const userAttributes = {
-            email: 'kaique.motta@hotmail.com',
-            given_name: 'Kaique',
-            family_name: 'Motta',
-            cpf: "469.622.488-06"
-        };
-        return {
-            version: "1",
-            userAttributes: userAttributes
-        };
-    } else {
-        throw new Error("Invalid trigger source");
-    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            name: 'Kaique',
+            surname:'Motta',
+            email: 'kaique.motta@hotmail.com'
+        }),
+    };
 };
