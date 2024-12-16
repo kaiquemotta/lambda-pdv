8f864defd33329ab036d918226aecfca0891722e
Kaique Motta <kaique.motta@hotmail.com>
2024-12-16 01:18:35 -0300

    feat: dev

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 exports.handler = async (event) => {
-    const userAttributes = {
-        email: 'kaique.motta@hotmail.com',
-        given_name: 'Kaique',
-        family_name: 'Motta',
-        cpf: "469.622.488-06"  // Aqui você coloca o CPF como atributo
-    };
+    // Verifica se o evento é do tipo TokenGeneration_HostedAuth
+    if (event.triggerSource === "TokenGeneration_HostedAuth") {
+        const userAttributes = {
+            email: 'kaique.motta@hotmail.com',
+            given_name: 'Kaique',
+            family_name: 'Motta',
+            cpf: "469.622.488-06"  // Aqui você coloca o CPF como atributo
+        };
 
-    return {
-        version: "1",  // A versão esperada pela resposta Lambda
-        userAttributes: userAttributes  // Retorna todos os atributos juntos
-    };
+        return {
+            version: "1",  // A versão esperada pela resposta Lambda
+            userAttributes: userAttributes  // Retorna todos os atributos juntos
+        };
+    } else {
+        throw new Error("Invalid trigger source");
+    }
 };
