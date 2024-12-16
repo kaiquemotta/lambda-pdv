1eac25a381c06fd86783e373471ea97539200ac5
Kaique Motta <kaique.motta@hotmail.com>
2024-12-15 23:57:00 -0300

    feat: dev

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            message: 'Hello from Lambda! Deploy 1',
-        }),
+        body: JSON.stringify(
+        [{
+            name: 'Kaique',
+            surname:'Motta',
+            email: 'kaique.motta@hotmail.com'
+        }]),
     };
 };
