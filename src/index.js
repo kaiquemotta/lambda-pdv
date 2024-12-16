c9e930902c5bde77f85ab12f9360725dc97e9ff7
Kaique Motta <kaique.motta@hotmail.com>
2024-12-16 01:15:31 -0300

    feat: dev

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 exports.handler = async (event) => {
+    const userAttributes = {
+        email: 'kaique.motta@hotmail.com',
+        given_name: 'Kaique',
+        family_name: 'Motta',
+        cpf: "469.622.488-06"  // Aqui você coloca o CPF como atributo
+    };
+
     return {
-        statusCode: 200,
-        body: JSON.stringify(
-        [{
-            name: 'Kaique',
-            surname:'Motta',
-            email: 'kaique.motta@hotmail.com'
-        }]),
+        version: "1",  // A versão esperada pela resposta Lambda
+        userAttributes: userAttributes  // Retorna todos os atributos juntos
     };
 };
