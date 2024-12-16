7b3c3c855e65e776eca77744a1b602f32e4b9031
Kaique Motta <kaique.motta@hotmail.com>
2024-12-16 13:23:03 -0300

    feat: dev

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 exports.handler = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            name: 'Kaique',
-            surname:'Motta',
-            email: 'kaique.motta@hotmail.com'
-        }),
+    // Logando o evento para depuração
+    console.log("Pre Token Generation event:", JSON.stringify(event));
+
+    // Exemplo de como adicionar uma afirmação (claim) personalizada ao token
+    const claimsToAddOrOverride = {
+        "custom:example_claim": "example_value", // Adiciona um claim personalizado
     };
+
+    // Estrutura da resposta com a versão "1" e claimsOverrideDetails
+    const response = {
+        version: "1",
+        payload: {
+            claimsOverrideDetails: {
+                claimsToAddOrOverride: claimsToAddOrOverride
+            }
+        }
+    };
+
+    return response;
 };
