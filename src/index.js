5b227b4c6380fa0da11ac451e22a37070b26f4dc
Kaique Motta <kaique.motta@hotmail.com>
2024-12-16 13:46:03 -0300

    feat: dev

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
-exports.handler = async (event) => {
-    console.log("Received event:", JSON.stringify(event)); // Imprime o evento recebido para depuração
-
-    // Verifica e imprime o tipo de trigger source
-    console.log("Trigger source:", event.triggerSource);
-
-    // Verifica se o evento é o trigger correto para a Token Generation
-    if (event.triggerSource !== "TokenGeneration_HostedAuth") {
-        console.error("Invalid trigger source:", event.triggerSource);
-        throw new Error("Invalid trigger source: Expected 'TokenGeneration_HostedAuth'");
-    }
-
-    // Retorna a resposta com a versão 2 e um payload vazio
-    const response = {
-        version: "2", // A versão correta para Pre Token Generation
-        payload: {}   // Nenhuma modificação no payload
+exports.handler = async (event, context) => {
+    event.response.claimsOverrideDetails = {
+         "claimsToAddOrOverride": {
+            "attribute_key2": "attribute_value2",
+            "attribute_key": "attribute_value"
+        }
     };
 
-    return response; // Retorna a resposta para o Cognito
+    return event
 };
