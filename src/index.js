d80bde349931ab5558d3c6c177f161d619607c0e
Kaique Motta <kaique.motta@hotmail.com>
2024-12-16 14:00:49 -0300

    feat: dev

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-// Função handler utilizando CommonJS (sem export)
 const handler = async (event) => {
-  // Adiciona ou sobrepõe claims no evento
   event.response = {
     claimsOverrideDetails: {
       claimsToAddOrOverride: {
         my_first_attribute: "first_value",
         my_second_attribute: "second_value",
       },
-      claimsToSuppress: ["email"], // Suprime o claim 'email'
+      claimsToSuppress: ["email"],
     },
   };
 
-  // Retorna o evento com a modificação dos claims
   return event;
 };
 
-// Exporte a função usando a sintaxe CommonJS
-module.exports.handler = handler;
+export { handler };
