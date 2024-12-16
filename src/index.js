76f7ffea1ebeceb2f17ef9be76f25ec7b772a436
Kaique Motta <kaique.motta@hotmail.com>
2024-12-16 01:25:10 -0300

    feat: dev

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 exports.handler = async (event) => {
-    // Verifica se o evento é do tipo TokenGeneration_HostedAuth
+    // Verifique se o triggerSource é o esperado para pre-token generation
     if (event.triggerSource === "TokenGeneration_HostedAuth") {
+        // Aqui você pode definir ou modificar os atributos do usuário
         const userAttributes = {
             email: 'kaique.motta@hotmail.com',
             given_name: 'Kaique',
             family_name: 'Motta',
-            cpf: "469.622.488-06"  // Aqui você coloca o CPF como atributo
+            cpf: "469.622.488-06"  // Coloque o CPF aqui, como desejado
         };
 
+        // Retorne a versão 1 e os atributos do usuário para o Cognito
         return {
-            version: "1",  // A versão esperada pela resposta Lambda
-            userAttributes: userAttributes  // Retorna todos os atributos juntos
+            version: "1",  // Versão esperada
+            userAttributes: userAttributes  // Retorna os atributos modificados
         };
     } else {
+        // Se o triggerSource não for o esperado, lance um erro
         throw new Error("Invalid trigger source");
     }
 };
