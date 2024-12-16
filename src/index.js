26f39d245ecc0d20fb66ccb57971d6fc4ab4d190
Kaique Motta <kaique.motta@hotmail.com>
2024-12-16 16:44:28 -0300

    feat: dev

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+const http = require('http');
+
 const handler = async (event) => {
   console.log('Evento recebido:', JSON.stringify(event));
 
-  // Simulando a validação do CPF
+  // Recuperando o CPF do evento
   const cpf = event.request.userAttributes['cognito:username']; // Ou outro campo onde o CPF é armazenado
   console.log('CPF a ser validado:', cpf);
 
-  // Aqui mockamos a resposta como se o CPF sempre existisse
-  const cpfExists = true; // Simula que o CPF sempre existe
+  let cpfExists = false; // Inicializa como false
+
+  try {
+    // Fazendo a chamada para o endpoint externo
+    const options = {
+      hostname: '44.220.133.153',
+      port: 8080,
+      path: `/customers/${cpf}`,
+      method: 'GET',
+    };
+
+    cpfExists = await new Promise((resolve, reject) => {
+      const req = http.request(options, (res) => {
+        if (res.statusCode === 200) {
+          resolve(true); // CPF encontrado
+        } else {
+          resolve(false); // CPF não encontrado
+        }
+      });
+
+      req.on('error', (e) => {
+        console.error('Erro na requisição:', e);
+        reject(false);
+      });
+
+      req.end();
+    });
+  } catch (error) {
+    console.error('Erro ao validar o CPF:', error.message);
+  }
 
   if (cpfExists) {
     console.log('CPF encontrado:', cpf);
